803c28dfffd6ca41206670ef90b4c94aa7bd51b2
renupaul <114790824+renupaul@users.noreply.github.com>
2022-10-01 09:03:41 +0530

    Create array sum practice question

---
+++ Task-4/arrays.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// function to return sum of elements in an array of size n
+int sum(int arr[], int n)
+{
+  // Insert code here
+	return 0;
+}
+
+int main()
+{
+	int arr[] = {12, 3, 4, 15};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << "Sum of given array is " << sum(arr, n);
+	return 0;
+}
