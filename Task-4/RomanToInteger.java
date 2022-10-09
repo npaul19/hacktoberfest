6ebd1226c8204d49b4dc451762273039ca6faa95
npaul19 <nishtha.paul@thoughtworks.com>
2022-10-09 16:58:18 +0530

    Add new practice Question

--- Task-4/RomanToInteger.java
+++ Task-4/RomanToInteger.java
@@ -1,27 +1,11 @@
-import java.util.*;
-class TwinPrime //recursive
+class RomanToInteger //recursive
 {
-  int isPrime(int n,int i)
+  static int convertToInteger(String str)
   {
-    if(i==n)
-      return 1;
-    else if(n%i==0 || n==1)
-      return 0;
-    else
-      return isPrime(n,i+1);
+    return 0;
   }
   public static void main(String args[])
   {
-    Scanner sc = new Scanner(System.in);
-    TwinPrime ob = new TwinPrime();
-    System.out.println("Enter the two numbers:\n");
-    int x= sc.nextInt();
-    int y= sc.nextInt();
-    int a= ob.isPrime(x,2);
-    int b= ob.isPrime(y,2);
-    if(a==1 && b==1 && Math.abs((x-y))==2)
-      System.out.println(x+" and " +y+" are Twin Primes\n");
-    else
-      System.out.println("Not twin primes\n");
+    System.out.println(convertToInteger("VIV"));
   }
 }
