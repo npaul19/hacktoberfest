c1b68f966f030a0c4a5ea1904711e45e23397e37
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2022-10-12 17:52:56 +0530

    Update Triangle2.java
    Added Triangle2 pattern.Could you pls review my changes @npaul19  ?

--- Task-5/Triangle2.java
+++ Task-5/Triangle2.java
@@ -7,6 +7,9 @@
 */
 public class Triangle2 {
   public static void main(String[] args){
-    // Write code here
+    for(int i=1;i<=5;i++){
+        for(int j=5;j>=i;j--)
+        System.out.print("* ");
+         System.out.println();
   }
 }
