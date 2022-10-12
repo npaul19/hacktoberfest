d538398d2a93efc210ede0467f593367d625d249
Nishtha Paul <76488641+npaul19@users.noreply.github.com>
2022-10-12 15:02:31 +0530

    Update MaxOccurrence.java

--- Task-4/MaxOccurrence.java
+++ Task-4/MaxOccurrence.java
@@ -28,6 +28,7 @@ class MaxOccurance {
   }
 
   public static void main(String args[]) {
+    // Find the word with max occurrences in the given sentence
     System.out.println(find("Bob hit a ball, the hit BALL flew far away after it was hit")); // Output : "hit"
     System.out.println(find("Bob hit a ball, the hit BALL flew far away when Bob threw the ball.")); // Output : "ball"
   }
