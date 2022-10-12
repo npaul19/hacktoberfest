d71e706c94e883c7eedbcdf68c8a5ae89ceda286
npaul19 <nishtha.paul@thoughtworks.com>
2022-10-12 23:49:09 +0530

    Updated diamond problem from java to c++

--- Task-5/DiamondPattern.cpp
+++ Task-5/DiamondPattern.cpp
@@ -9,8 +9,12 @@
       *   *
         *
 */
-public class DiamondPattern {
-  public static void main(String[] args){
+#include <iostream>
+using namespace std;
+
+int main()
+{
     // Write code here
-  }
+    
+    return 0;
 }
