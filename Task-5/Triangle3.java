99bd0ee9cafa0073ec65fb28ebf1ccb6841d39fa
Nishtha Paul <76488641+npaul19@users.noreply.github.com>
2022-10-12 23:02:01 +0530

    Update Triangle3.java

--- Task-5/Triangle3.java
+++ Task-5/Triangle3.java
@@ -10,7 +10,7 @@ public class Triangle3 {
  
       for(int i=1;i<=5;i++){
      for(int j=1;j<i;j++)
-      System.out.print(" "+"");
+      System.out.print(" "+" ");
       for(int k=1;k<=(5-i+1);k++)
       System.out.print("*"+" ");
       System.out.println();
