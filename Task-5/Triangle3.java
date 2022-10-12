1e495c5a755d856480fbe8c5903dbf0760954998
Nishtha Paul <76488641+npaul19@users.noreply.github.com>
2022-10-12 23:04:06 +0530

    Update Triangle3.java

--- Task-5/Triangle3.java
+++ Task-5/Triangle3.java
@@ -7,14 +7,13 @@
 */
 public class Triangle3 {
   public static void main(String[] args){
- 
-      for(int i=1;i<=5;i++){
-     for(int j=1;j<i;j++)
-      System.out.print(" "+" ");
-      for(int k=1;k<=(5-i+1);k++)
-      System.out.print("*"+" ");
+    // Write code here
+    for(int i=1; i<=5; i++){
+      for(int j=1; j<i; j++)
+      System.out.print(" " + " ");
+      for(int k=1; k<=(5-i+1); k++)
+      System.out.print("*" + " ");
       System.out.println();
-     }
-  }}
- 
-
+    }
+  }
+}
