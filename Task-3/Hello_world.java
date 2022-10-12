42f4e6a8aa103630fe1b34a3df9408a19b21beda
srikresna <srikresna.dewa@gmail.com>
2022-10-12 18:05:47 +0700

    added new helloworld file

---
+++ Task-3/Hello_world.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Hello_world {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String name;
+
+        System.out.print("Input your name\t= ");
+        name = input.nextLine();
+
+        System.out.println("\nHello World! My name is " + name);
+    }
+}
