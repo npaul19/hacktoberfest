a6694ab0b3caba27147768281de750f10219733c
sandeep <107111616+sandeepB3@users.noreply.github.com>
2022-10-12 23:43:58 +0530

    Java code for Triangle Pattern 10

--- Task-5/Triangle10.java
+++ Task-5/Triangle10.java
@@ -8,5 +8,28 @@
 public class Triangle10 {
   public static void main(String[] args){
     // Write code here
+   int rows = 5;  //Row of 5 stars
+   
+   //Upper stars 5 --> 3 --> 1
+   for (int i=0; i<rows; i=i+2){
+    for (int j=0; j<i; j++){
+     System.out.print(" ");
+    }
+    for (int k=i; k<rows; k++){
+     System.out.print("*" + " ");
+    } 
+    System.out.println("");
+   } 
+   
+   //Lower stars 3 --> 5
+   for (int i=rows-3; i>=0; i=i-2){
+    for (int j=0; j<i ;j++){
+     System.out.print(" ");
+    }
+    for (int k=i; k<rows; k++){
+     System.out.print("*" + " ");
+    }
+    System.out.println("");
+   }
   }
 }
