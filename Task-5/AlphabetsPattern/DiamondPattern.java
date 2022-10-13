e69da44d3f062de94d9876893d7065c8586fabc7
Moksh Kori <41103527+mokshkori@users.noreply.github.com>
2022-10-13 12:39:26 +0530

    Update DiamondPattern.java

--- Task-5/AlphabetsPattern/DiamondPattern.java
+++ Task-5/AlphabetsPattern/DiamondPattern.java
@@ -11,8 +11,36 @@ E               E
         A
 */
 public class DiamondPattern {
+    static private void printSpaces(int n){
+        for(int i = 0 ; i < n; i ++) System.out.print(" ");
+    }
     public static void main(String[] args) {
-        // Write code here
+        int n = 5;
+            
+        //UPPER half
+        for(int row = 1 ; row <= n ; row++){
+            char base = (char)('A' + row - 1);
+            printSpaces(2 * (n - row));
+            System.out.print(base);
 
+            if(row > 1){
+                printSpaces(4 * row - 5);
+                System.out.print(base);
+            }
+            System.out.println();
+        }
+            
+        // LOWER half
+        for(int row = n - 1 ; row >= 1 ; row --){
+            char base = (char)('A' + row - 1);
+            printSpaces(2 * (n - row));
+            System.out.print(base);
+
+            if(row > 1){
+                printSpaces(4 * row - 5);
+                System.out.print(base);
+            }
+            System.out.println();
+        }
     }
 }
