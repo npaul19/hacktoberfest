d28d8d1d0a16db4aadd839f1716f4387a1dbcaa9
Nishad Ahamed <94828211+nish-ahmd-it@users.noreply.github.com>
2022-10-13 20:32:30 +0530

    Update Triangle7.java
    Added the algorithm for the current problem

--- Task-5/Triangle7.java
+++ Task-5/Triangle7.java
@@ -5,8 +5,25 @@
       * * *
         *
 */
-public class Triangle7 {
-  public static void main(String[] args){
-    // Write code here
-  }
+public class Trinagle7 {
+
+    public static void main(String[] args) {
+	// write your code here
+        int rows=9;
+        for (int i= 0; i<= rows-1; i++)
+        {
+            for (int j=0; j<=i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k=0; k<=rows-1-i; k++)
+            {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+
+        }
+
+
+    }
 }
