572ae49e209b9f9cfa1b53e212a303ca30c9baf9
npaul19 <nishtha.paul@thoughtworks.com>
2022-10-13 11:13:18 +0530

    Reformat

--- Task-5/Triangle12.java
+++ Task-5/Triangle12.java
@@ -1,3 +1,4 @@
+
 /* Triangle Pattern 12
 *       *
   *   *
@@ -5,21 +6,24 @@
   *   *
 *       *
 */
+import java.util.Scanner;
+
 public class Triangle12 {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int height;
     System.out.println("Enter height: ");
     height = in.nextInt();
-   
-   for(int i =1; i< height + 1; i++){
-    for(int j = 1; j< height + 1; j++){
-     if(i == j || i + j == height + 1)
-      System.out.print("*");
-     else
-      System.out.print(" ");
+
+    for (int i = 1; i < height + 1; i++) {
+      for (int j = 1; j < height + 1; j++) {
+        if (i == j || i + j == height + 1)
+          System.out.print("* ");
+        else
+          System.out.print("  ");
+      }
+      System.out.println();
+      in.close();
     }
-    System.out.println();
-   }
   }
 }
