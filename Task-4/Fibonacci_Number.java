aa66e44b046a3d4d78f861f2e5ad189b5fe60412
Kiran <59613353+bishtkiran@users.noreply.github.com>
2022-10-13 10:03:22 +0530

    Add FibonacciNumber.java
    Add code to find nth number in a fibonacci series.

---
+++ Task-4/Fibonacci_Number.java
@@ -0,0 +1,26 @@
+//Program to find nth Fibonacci Number
+public class FibonacciNumber{
+  public static void main(String[] args){
+    int number = 8;
+    System.out.println("Nth number in the series is: "+findFib(number));
+  }
+  
+  public static int findFib(int number){
+    if(number < 2)
+      return number;
+    int first = 0;
+    int second = 1;
+    int sum =0;
+    for(int i =2; i< number + 1; i++){
+      sum = first + second;
+      first = second;
+      second = sum;
+    }
+    return sum;
+  }
+}
+    
+
+
+
+//prints.  Nth number in the series is: 21
