95b926e88857ba90d70eb9381f402bc94260f46b
Moksh Kori <41103527+mokshkori@users.noreply.github.com>
2022-10-13 12:15:20 +0530

    Update BinaryPattern1.java

--- Task-5/NumbersPattern/BinaryPattern1.java
+++ Task-5/NumbersPattern/BinaryPattern1.java
@@ -8,7 +8,15 @@ package NumbersPattern;
 */
 public class BinaryPattern1 {
     public static void main(String[] args) {
-        // Write code here
-        
+        int n = 5;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j ++){
+                if((i + j) % 2 == 0)
+                    System.out.print("1 ");
+                else
+                    System.out.print("0 ");
+            }
+            System.out.println();
+        }
     }
 }
