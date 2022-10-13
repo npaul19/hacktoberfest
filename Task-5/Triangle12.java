d23021998ec7de9415f3419ea7c79a32e2e5ee34
Kiran <59613353+bishtkiran@users.noreply.github.com>
2022-10-13 10:32:12 +0530

    Update Triangle12.java
    Add program to print cross pattern in java.

--- Task-5/Triangle12.java
+++ Task-5/Triangle12.java
@@ -7,6 +7,19 @@
 */
 public class Triangle12 {
   public static void main(String[] args){
-    // Write code here
+    Scanner in = new Scanner(System.in);
+    int height;
+    System.out.println("Enter height: ");
+    height = in.nextInt();
+   
+   for(int i =1; i< height + 1; i++){
+    for(int j = 1; j< height + 1; j++){
+     if(i == j || i + j == height + 1)
+      System.out.print("*");
+     else
+      System.out.print(" ");
+    }
+    System.out.println();
+   }
   }
 }
